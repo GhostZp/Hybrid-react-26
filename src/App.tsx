3c50b378f4ded23bdbba39f527414e92072a650a
GhostZp <amsku.kallio@gmail.com>
2026-01-19 11:03:09 +0200

    First component ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css'
+//import './App.css'
+import Home from './components/Home';
 
 const App = () => {
 
   return (
   <>
-    <h1>My App</h1>
-    <p>Hello!</p>
+    <h1>My Media Sharing App</h1>
+    <Home />
   </>
   );
 }
